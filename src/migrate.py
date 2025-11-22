7c428508aa8ab2f52e220a1fa64de815ce4b49d2
Ricardo Villarreal <ricar6144@gmail.com>
2025-11-22 17:16:48 -0500

    fix migration

--- src/migrate.py
+++ src/migrate.py
@@ -1,5 +1,5 @@
 from config import Config
-from model import UserMigration, TrackMigration, CategoryMigration
+from model import UserMigration, TrackMigration
 from model import ArtistMigration, AlbumMigration, PlaylistMigration
 from model import ArtistTrackMigration, PlaylistTrackMigration, FileMigration
 
@@ -13,9 +13,6 @@ if __name__ == "__main__":
     
     if not TrackMigration().create():
         print("Error to create table tracks")
-
-    if not CategoryMigration().create():
-        print("Error to create table categories")
 
     if not ArtistMigration().create():
         print("Error to create table artists")
